4a0e63711b2a5b5acf2124acdbd335edaf852b80
Matheus Godoy <signatus@gmail.com>
2022-11-22 20:15:32 -0300

    <chore> adição de update e delete items

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ const listItems = document.querySelector("#listItems");
 const itemsCollection = JSON.parse(localStorage.getItem("items")) || [];
 
 itemsCollection.forEach((e) => {
-    addItem(e.name, e.quantity);
+    addItem(e);
 });
 
 form.addEventListener("submit", (e) => {
@@ -12,25 +12,66 @@ form.addEventListener("submit", (e) => {
     const nameItem = e.target.elements["nome"];
     const qntItem = e.target.elements["quantidade"];
 
-    itemsCollection.push({ name: nameItem.value, quantity: qntItem.value });
-    localStorage.setItem("items", JSON.stringify(itemsCollection));
-    
-    addItem(nameItem.value, qntItem.value);
+    const currentItem = {
+        name: nameItem.value,
+        qntItem: qntItem.value,
+    };
 
+    const validatingItem = itemsCollection.find((e) => e.name === nameItem.value);
+
+    if (validatingItem) {
+        currentItem.id = validatingItem.id;
+        updateItem(currentItem);
+        itemsCollection[itemsCollection.findIndex(e => e.id === validatingItem.id)] = currentItem
+    } else {
+        currentItem.id = itemsCollection[itemsCollection.length - 1] ? (itemsCollection[itemsCollection.length -1]).id + 1 : 0
+        addItem(currentItem);
+        itemsCollection.push(currentItem);
+    }
+
+    localStorage.setItem("items", JSON.stringify(itemsCollection));
     nameItem.value = "";
     qntItem.value = "";
 });
 
-function addItem(name, qnt) {
+function updateItem(currentItem) {
+    document.querySelector(`[data-id="${currentItem.id}"]`).innerHTML = currentItem.qntItem;
+}
+
+function addItem(currentItem) {
     const newItem = document.createElement("li");
     newItem.classList.add("item");
 
-    const itemQnt = document.createElement("strong");
-    itemQnt.innerHTML = qnt;
-
+    const itemQnt = setItemQuantity(currentItem);
+    const removeButton = createRemoveButton(currentItem.id);
     newItem.appendChild(itemQnt);
-    newItem.innerHTML += name;
+    newItem.innerHTML += currentItem.name;
+    newItem.appendChild(removeButton);
 
     listItems.appendChild(newItem);
+}
 
-};
+function setItemQuantity(currentItem) {
+    const itemQnt = document.createElement("strong");
+    itemQnt.innerHTML = currentItem.qntItem;
+    itemQnt.dataset.id = currentItem.id;
+
+    return itemQnt;
+}
+
+function createRemoveButton(id) {
+    const buttonElement = document.createElement("button");
+    buttonElement.innerText = "X";
+
+    buttonElement.addEventListener("click", function () {
+        deleteItem(this.parentNode, id);
+    });
+
+    return buttonElement;
+}
+
+function deleteItem(element, id) {
+    element.remove();
+    itemsCollection.splice(itemsCollection.findIndex(e => e.id === id), 1)
+    localStorage.setItem("items", JSON.stringify(itemsCollection));
+}
